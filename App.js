b53b3dcb4da1d8182dd234252be8e6588e915cf8
6derick6 <6derick6@gmail.com>
2023-05-03 12:46:53 -0300

    App Music 1/5

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Audio } from 'expo-av';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      <StatusBar hidden/>
+      <View style={styles.header}>
+        <Text style={{textAlign:'center',color:'white',fontSize:25}}>Player Music</Text>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#222',
   },
+
+  header: {
+    backgroundColor:'#1DB954',
+    width:'100%',
+    padding:20
+  }
 });
