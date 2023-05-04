3a2d37f771e683fca7f33909e985280ccf3455ad
6derick6 <6derick6@gmail.com>
2023-05-04 12:53:34 -0300

    App Music 2/5

--- App.js
+++ App.js
@@ -1,14 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Audio } from 'expo-av';
+import { useState } from 'react';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function App() {
+
+  const [audio,setarAudio] = useState(null);
+
+  const [musicas,setarMusicas] = useState([
+
+    {
+      nome: 'Sweet chilf of mine',
+      artista: 'Guns N Roses',
+      playing: true,
+      file: ''
+    },
+
+    {
+      nome: 'Welcome to the jungle',
+      artista: 'Guns N Roses',
+      playing: false,
+      file: ''
+    },
+
+    {
+      nome: 'This love',
+      artista: 'Maroon 5',
+      playing: false,
+      file: ''
+    }
+
+  ]);
+
   return (
     <ScrollView style={styles.container}>
       <StatusBar hidden/>
       <View style={styles.header}>
         <Text style={{textAlign:'center',color:'white',fontSize:25}}>Player Music</Text>
       </View>
+
+      <View style={styles.table}>
+        <Text style={{width:'50%', color:'rgb(200,200,200)'}}>Múscia</Text>
+        <Text style={{width:'50%', color:'rgb(200,200,200)'}}>Artista</Text>
+      </View>
+
+      {
+        musicas.map((val)=>{
+
+          if(val.playing){
+            return(
+              <View style={styles.table}>
+                <TouchableOpacity style={{width:'100%',flexDirection:'row'}}>
+                  <Text style={styles.tableTextSelected}><AntDesign name='play' size={15} color="#1DB954" />  {val.nome}</Text>
+                  <Text style={styles.tableTextSelected}>{val.artista}</Text>
+                </TouchableOpacity>
+              </View>
+            );
+          }else{
+            return(
+              <View style={styles.table}>
+                <TouchableOpacity style={{width:'100%',flexDirection:'row'}}>
+                  <Text style={styles.tableText}><AntDesign name='play' size={15} color="white" />  {val.nome}</Text>
+                  <Text style={styles.tableText}>{val.artista}</Text>
+                </TouchableOpacity>
+              </View>
+            );
+          }
+
+        })
+      }
+
     </ScrollView>
   );
 }
@@ -23,5 +85,22 @@ const styles = StyleSheet.create({
     backgroundColor:'#1DB954',
     width:'100%',
     padding:20
+  },
+
+  table: {
+    flexDirection:'row',
+    padding:20,
+    borderBottomColor:'white',
+    borderBottomWidth:1
+  },
+
+  tableTextSelected: {
+    width:'50%',
+    color:'#1DB954'
+  },
+
+  tableText: {
+    width:'50%',
+    color:'white'
   }
 });
